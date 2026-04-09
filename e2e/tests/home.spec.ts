66a8cf132dabe78ea7436110e823b989e22468c5
threezinedine <threezinedine@gmail.com>
2026-04-09 22:36:59 +0700

    [feature] add formatter

--- e2e/tests/home.spec.ts
+++ e2e/tests/home.spec.ts
@@ -1,8 +1,8 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test'
 
-test.describe("Home Page", () => {
-  test("should display the correct title", async ({ page }) => {
-    await page.goto("/");
-    await expect(page).toHaveTitle(/client/);
-  });
-});
+test.describe('Home Page', () => {
+	test('should display the correct title', async ({ page }) => {
+		await page.goto('/')
+		await expect(page).toHaveTitle(/client/)
+	})
+})
