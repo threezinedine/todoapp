fc8b45c1967a380ce0fa45a655b48549e92bfa0c
threezinedine <threezinedine@gmail.com>
2026-04-17 08:35:43 +0700

    [feature] complete first navbar with normal login navbar example

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,10 +1,17 @@
 import type { NavbarProps } from './NavbarProps'
 import styles from './Navbar.module.scss'
+import { MenuToggle } from '../menu-toggle'
 
 export function Navbar({ icon, rightSide }: NavbarProps) {
 	return (
 		<div className={styles.navbar}>
 			<div className={styles['left-side']}>
+				<div className={styles['menu-toggle']}>
+					<MenuToggle
+						isOpen={false}
+						onClick={() => {}}
+					/>
+				</div>
 				<div className={styles.logo}>{icon}</div>
 				<div className={styles.branch}>TodoApp</div>
 			</div>
